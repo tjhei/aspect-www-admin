25b59c51740bc06bb7e403f19885c20da145e4db
Timo Heister <timo.heister@gmail.com>
2017-12-21 08:04:22 -0500

    docu

--- update.sh
+++ update.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# update the two repos and triggers
+# 1. update of the www/ directory from www-repo/ (www branch in ASPECT repo)
+# 2. update the doxygen documentation and put it into www/doc/
+# 3. build www/publications.html from aspect-repo/doc/manual/citing_aspect.bib
+#
+# Note: you can force a build by running ``FORCE=1 ./update.sh``
+#
+# Requirements: tjhei/dealii-doc-gen and tjhei/dealii-java-jabref docker images
+
 PUB=`cd www;pwd;cd ..`
 CHANGED=0
+
+if [ ! -z "$FORCE" ]
+then
+    echo "you FORCEd a regen..."
+    CHANGED=1
+fi
 
 cd www-repo
 LASTREV=`git log -1 --format=format:%H`
