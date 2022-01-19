d16fd2bd745161112a3f696059f31616b06cc88f
Timo Heister <timo.heister@gmail.com>
2022-01-19 13:50:28 -0500

    rename master to main

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --single-branch --branch master https://github.com/geodynamics/aspect.git aspect-repo
+git clone --single-branch --branch main https://github.com/geodynamics/aspect.git aspect-repo
 
 git clone --single-branch --branch www https://github.com/geodynamics/aspect.git www-repo
 
